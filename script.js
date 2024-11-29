c54978d0fc7b5b465d6db4c95790698760077275
afsona571 <afsonaa59@gmail.com>
2024-11-29 18:19:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// let arr = ['banan', 'shaftoli', 'olma', 'nok']
+// console.log(arr);
+
+// alert('array uzunkigi ' + arr.length)
+
+// let con = confirm('biz arraydan bita meva aymoqchimiz ')
+// if(con){
+//     arr.pop()
+// }
+
+
+
+// console.log(arr);
+let number = [1, 2, 3, 4,65,70,80,56,60]
+let juft = []
+// метод = бу обект ичида очилган функция 
+3 /2 
+// jusft sondan qoldik kolmaydi
+
+number.forEach(item =>{
+    
+    if(item % 2 === 0){
+juft.push(item)
+    }
+    
+    
+})
+
+console.log(number +'boshlangich holatdagi array');
+console.log(juft + 'juft sonlik array');
+
